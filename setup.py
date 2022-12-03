fabdca428fa54e6a0663becb6c60f7a05f053da0
dudushy <eduardotalarico1337@gmail.com>
2022-12-03 18:07:03 -0300

    Remove old project

--- setup.py
+++
@@ -1,6 +0,0 @@
-# pip freeze > requirements.txt
-#* -- Imports
-import os
-
-os.system("pip install -r requirements.txt") #? Install requirements
-os.system("pause") #? Send "pause" to system (Press any key to continue . . .)
