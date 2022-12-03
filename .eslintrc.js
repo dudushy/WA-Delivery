8b63dbb6425d42f8dd86c43f64a0454752cd8cd7
dudushy <eduardotalarico1337@gmail.com>
2022-12-03 18:15:52 -0300

    `npm init @eslint/config`

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
