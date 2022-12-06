86e1e3c2006705c44a59237a5af20f409bc28a2a
dudushy <eduardotalarico1337@gmail.com>
2022-12-05 21:46:12 -0300

    Rename client -> bot

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const fs = require('fs');
 
-const client = new Client({
+const bot = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: process.argv[2] == '--show' ? false : true,
@@ -30,38 +30,38 @@ function updateData(newData) {
 }
 
 const exit = () => {
-  client.destroy();
+  bot.destroy();
   console.log('[bot] finished.');
 };
 
-client.on('qr', qr => {
+bot.on('qr', qr => {
   console.log('[bot#qr] generating...');
   qrcode.generate(qr, { small: true });
 
   updateData([]);
 });
 
-client.on('loading_screen', (percent, message) => {
+bot.on('loading_screen', (percent, message) => {
   console.log(`[bot#loading_screen] ${percent}%`, message);
 });
 
-client.on('authenticated', (session) => {
+bot.on('authenticated', (session) => {
   console.log('[bot#authenticated] client is authenticated!', session);
 });
 
-client.on('auth_failure', error => {
+bot.on('auth_failure', error => {
   console.error('[bot#auth_failure] ERROR', error);
 });
 
-client.on('ready', () => {
+bot.on('ready', () => {
   console.log('[bot#ready] client is ready!');
 });
 
-client.on('disconnected', (reason) => {
+bot.on('disconnected', (reason) => {
   console.log('[bot#disconnected] client disconnected', reason);
 });
 
-client.on('message', msg => {
+bot.on('message', msg => {
   console.log('[bot#message] received', msg);
 
   if (msg.body == '!ping') {
@@ -89,7 +89,7 @@ fs.readFile('./data.json', 'utf8', (err, jsonString) => {
   }
 });
 
-client.initialize();
+bot.initialize();
 
 process.on('SIGINT', exit);  // CTRL+C
 process.on('SIGQUIT', exit); // Keyboard quit
